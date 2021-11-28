53206a33fa87d8b4d0ba25639469fca80bb94d3a
haomingjue <haomingjue@outlook.com>
2021-11-27 23:42:12 -0500

    leetcode-practice

---
+++ java/56_Merge-Intervals/56_Merge-Intervals.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        ArrayList<int[]> list = new ArrayList<>();
+        Arrays.sort(intervals, (arr1, arr2) -> Integer.compare(arr1[0], arr2[0]));
+        boolean flag = false;
+        for (int i = 0; i < intervals.length; i++) {
+            int[] temp = intervals[i];
+            while (i < intervals.length && intervals[i][0] <= temp[1]) {
+                flag = true;
+                temp[1] = Math.max(intervals[i][1], temp[1]);
+                i++;
+            }
+            if (flag) {
+                i--;
+                flag = false;
+            }
+            list.add(temp);
+        }
+        int[][] ret = new int[list.size()][2];
+        list.toArray(ret);
+        return ret;
+    }
+}
